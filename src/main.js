0914a10d4dee7a424483c6e14226c9407bed508c
”Jakob <jako500n@stud.kea.dk>
2022-09-13 22:13:14 +0200

    Current Webfeature

--- src/main.js
+++ src/main.js
@@ -1,1 +1,39 @@
 import "./style.css";
+/* Import af Motion One bibliotek */
+import { animate, stagger, inView, scroll, timeline } from "motion";
+
+document
+  .querySelectorAll(".parallaxcontainermedflereelementer")
+  .forEach((parallaxcontainer) => {
+    const elementderskalparallaxes2 =
+      parallaxcontainer.querySelector(".paragraf_01");
+    scroll(animate(elementderskalparallaxes2, { y: [0, 800] }), {
+      target: elementderskalparallaxes2,
+    });
+  });
+
+scroll(
+  animate(".scrollzoomimg", {
+    scale: [1, 9],
+  }),
+
+  {
+    target: document.querySelector(".scrollzoomimg"),
+
+    offset: ["0.25 0.25", "1 1"],
+  }
+);
+
+const lillevideo = document.querySelector("#videoscrub");
+lillevideo.pause();
+
+scroll(
+  ({ y }) => {
+    if (!lillevideo.readyState) return;
+    lillevideo.currentTime = lillevideo.duration * y.progress;
+  },
+  {
+    target: document.querySelector(".videoarticle"),
+    offset: ["-100vh", "end end"],
+  }
+);
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#E38B29",
+        "purple-dark": "#000131",
+        hvid: "#FDFDFD",
+        blaa: "#06EEF2",
+        groen: "#04EA11",
+        lyseroedLight: "#E557B6",
+        lyseroedDark: "#FE00F5",
+        bygning: "#95A3AD",
+      },
+    },
+    fontFamily: {
+      notoSans: "Noto Sans, sans-serif",
+      oswald: "Oswald, sans-serif",
+      inter: "Inter, sans-serif",
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
